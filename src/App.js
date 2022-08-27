d148bb9daf5f9566ff25d4e7fd7fdaf00d0879d3
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2022-08-27 12:00:58 -0300

    Fixed Router in App.js
     (rutas con "to", deben ser "path")

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {Routes, Route} from "react-router-dom"
 const App = () => {
   return (
     <Routes>
-      <Route to="/" element={<Home />} />
-      <Route to="/login" element={<Login />} />
-      <Route to="/register" element={<Register />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route to="/cart" element={<Cart />} />
     </Routes>
   )
