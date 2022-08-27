e46103ef1c827f5fc0fd49972bc687b8c0bac65e
Tincho develops <martinlabastie.p@gmail.com>
2022-08-27 11:58:44 -0300

    Login Functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Routes, Route} from "react-router-dom"
+import Login from "./auth/login"
+import Register from "./auth/Register"
+import Home from "./components/Home/Home"
+
 
 const App = () => {
   return (
     <Routes>
-      <Route to="/" element={<Home />} />
-      <Route to="/login" element={<Login />} />
-      <Route to="/register" element={<Register />} />
-      <Route to="/cart" element={<Cart />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      {/* <Route path="/cart" element={<Cart />} /> */}
     </Routes>
   )
 }
