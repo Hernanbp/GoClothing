9f02f5f849c06cfbc22e289bb0d5b64006b9464d
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-08-29 14:38:54 -0300

    fixes header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import { Footer } from './components/views/Footer/Footer'
-import { Header } from './components/views/Header/Header'
+// import { Header } from './components/views/Header/Header'
 import './styles.css'
 
 const App = () => {
 
   return (
-    <Header />
+    // <Header />
+    <>
+    </>
   )
 }
 
