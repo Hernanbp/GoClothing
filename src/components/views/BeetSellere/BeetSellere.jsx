6cbaf13c35f1e0af00d41cffc03a22ecffa54362
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-08-31 15:41:25 -0300

    add backgorund img and edit styles

--- src/components/views/BeetSellere/BeetSellere.jsx
+++ src/components/views/BeetSellere/BeetSellere.jsx
@@ -1,3 +1,4 @@
+import "./beetSellereStyles.css"
 
 export function BeetSellere() {
   return (
@@ -5,32 +6,32 @@ export function BeetSellere() {
       <section className="beetSellere-section">
         <div className="beetSellere-header">
             <div>
-                <img src="" alt="" />
-                <div>
-                    <h1>ZARA</h1>
-                    <p>TEXTOdasdasdasdsa</p>
-                    <button>See Collection</button>
-                </div>
+                <h1>ZARA</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur id accusantium minima. Dolores ratione, asperiores sint numquam.</p>
+                <button>See Collection</button>
             </div>
         </div>
-        <div className="">
-            <h2>Best Sellers</h2>
+        <div className="beetSellere-main">
             <div>
+                <h2>Best Sellers</h2>
                 <div>
-                    <ul>
-                        <li>All Products</li>
-                        <li>T-Shirt</li>
-                        <li>Jacket</li>
-                    </ul>
-                    <button>Show All</button>
-                </div>
-                <div>
-                    las 4 CARDS
+                    <div>
+                        <ul>
+                            <li>All Products</li>
+                            <li>T-Shirt</li>
+                            <li>Jacket</li>
+                        </ul>
+                        <button>Show All</button>
+                    </div>
+                    <div>
+                        las 4 CARDS
+                    </div>
                 </div>
             </div>
+            
         </div>
         <div className="">
-
+                ES COMO EL FOOTER NUEVO
         </div>
       </section>
     </>
