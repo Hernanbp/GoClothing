2df0c8c3b32d0cc87a045b7b98ea9d21e5b5bc5e
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-08-31 17:28:19 -0300

    adding styles for beetSellere-header

--- src/components/views/BeetSellere/BeetSellere.jsx
+++ src/components/views/BeetSellere/BeetSellere.jsx
@@ -5,6 +5,13 @@ export function BeetSellere() {
     <>
       <section className="beetSellere-section">
         <div className="beetSellere-header">
+            {
+                /*
+                Es la palabra ZARA, de fondo, GRIS
+                <img src="./img/zaraLogo.png" alt="NO EXISTE LA IMG" />
+                */
+            }
+            
             <div>
                 <h1>ZARA</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur id accusantium minima. Dolores ratione, asperiores sint numquam.</p>
@@ -30,8 +37,21 @@ export function BeetSellere() {
             </div>
             
         </div>
-        <div className="">
-                ES COMO EL FOOTER NUEVO
+        <div className="beetSellere-footer">
+               <h2>Follow Products And Discounts On Instagram</h2>
+               <ul>
+                <li>foto</li>
+                <li>foto</li>
+                <li>foto</li>
+               </ul>
+               <h3>Or Suscribe To The Newsletter</h3>
+               <div>
+                    <p>
+                    Lorem ipsum dolor.
+                    </p>
+                    <button>SUBMIT</button>
+                </div>
+               
         </div>
       </section>
     </>
