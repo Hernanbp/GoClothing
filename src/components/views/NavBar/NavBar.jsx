99b144cd267bc779d92b8ac26723c9b6b57ec778
Femar 01 <femar01@femar.local>
2022-09-01 16:17:36 -0300

    navbar style  changes

--- src/components/views/NavBar/NavBar.jsx
+++ src/components/views/NavBar/NavBar.jsx
@@ -1,48 +1,43 @@
 import "./styles.css"
-import {useState} from "react"
-import PersonIcon from '@mui/icons-material/Person';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import SearchIcon from '@mui/icons-material/Search';
-import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react"
+import PersonIcon from "@mui/icons-material/Person"
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag"
+import SearchIcon from "@mui/icons-material/Search"
+import MenuIcon from "@mui/icons-material/Menu"
 import logo from "../../svgs/logo.svg"
-import { useResize } from "../../hooks/useResize";
-
-
+import { useResize } from "../../hooks/useResize"
 
 export function NavBar() {
-  const [show, setShow]=useState()
-  const {phoneScreen} = useResize();
-
-
-
+  const [show, setShow] = useState()
+  const { phoneScreen } = useResize()
 
   return (
-    <nav className="navbar">
-        <div className="top"> 
+    <div className="nav-container">
+      <nav className="navbar">
+        <div className="top">
           {<SearchIcon />}
           <img src={logo} alt="e-commerce" />
-          <div> 
+          <div>
             {<PersonIcon />}
             {<ShoppingBagIcon />}
-            
           </div>
         </div>
 
-        {phoneScreen ? 
-          (<div className="bottom-phone-show">
+        {phoneScreen ? (
+          <div className="bottom-phone-show">
             <MenuIcon />
-          </div>)
-        :
-          (<div className="bottom"> 
+          </div>
+        ) : (
+          <div className="bottom">
             <p>Jewelry & Accessories</p>
-            <p>Clothing & Shoes</p>  
-            <p>Home & Living</p>  
-            <p>Wedding & Party</p>  
+            <p>Clothing & Shoes</p>
+            <p>Home & Living</p>
+            <p>Wedding & Party</p>
             <p>Toys & Entertainment</p>
             <p>Art & Collectibles </p>
             <p>Craft Supplies & Tools </p>
-          </div>)
-        }
+          </div>
+        )}
 
         {/* <div className="bottom"> 
           <p>Jewelry & Accessories</p>
@@ -53,6 +48,7 @@ export function NavBar() {
           <p>Art & Collectibles </p>
           <p>Craft Supplies & Tools </p>
         </div> */}
-    </nav>
+      </nav>
+    </div>
   )
 }
