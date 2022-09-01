4721d2bd0abd1fe9e4c6c5e6dca8659504b1c303
Tincho develops <martinlabastie.p@gmail.com>
2022-09-01 12:19:15 -0300

    initial responsive (hook)

--- src/components/views/NavBar/NavBar.jsx
+++ src/components/views/NavBar/NavBar.jsx
@@ -1,25 +1,50 @@
 import "./styles.css"
+import {useState} from "react"
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from "../../svgs/logo.svg"
+import { useResize } from "../../hooks/useResize";
 
 
 
 export function Navbar() {
+  const [show, setShow]=useState()
+  const {phoneScreen} = useResize();
+
+
+
+
   return (
     <nav className="navbar">
         <div className="top"> 
           {<SearchIcon />}
-          <img src={logo} alt />
+          <img src={logo} alt="e-commerce" />
           <div> 
             {<PersonIcon />}
             {<ShoppingBagIcon />}
-            {/* {<MenuIcon />}       */}
+            
           </div>
         </div>
-        <div className="bottom"> 
+
+        {phoneScreen ? 
+          (<div className="bottom-phone-show">
+            <MenuIcon />
+          </div>)
+        :
+          (<div className="bottom"> 
+            <p>Jewelry & Accessories</p>
+            <p>Clothing & Shoes</p>  
+            <p>Home & Living</p>  
+            <p>Wedding & Party</p>  
+            <p>Toys & Entertainment</p>
+            <p>Art & Collectibles </p>
+            <p>Craft Supplies & Tools </p>
+          </div>)
+        }
+
+        {/* <div className="bottom"> 
           <p>Jewelry & Accessories</p>
           <p>Clothing & Shoes</p>  
           <p>Home & Living</p>  
@@ -27,7 +52,7 @@ export function Navbar() {
           <p>Toys & Entertainment</p>
           <p>Art & Collectibles </p>
           <p>Craft Supplies & Tools </p>
-        </div>
+        </div> */}
     </nav>
   )
 }
