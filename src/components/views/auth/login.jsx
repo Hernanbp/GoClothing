2621e2ec35178f051d2a3a4733470de7c022303f
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-09-02 18:27:25 -0300

    styles login

--- src/components/views/auth/login.jsx
+++ src/components/views/auth/login.jsx
@@ -5,9 +5,20 @@ import { useFormik } from "formik"
 import * as Yup from 'yup';
 import Swal from 'sweetalert2';
 import "./auth.styles.css"
+import logo from '../../svgs/logo.svg'
 
 
 export const Login = () => {
+
+  const linkStyle = {
+    textDecoration: 'none',
+    fontWeight: 600,
+    fontSize: '13px',
+    color: `rgba(${30}, ${40}, ${50}, ${0.8})`,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    marginLeft: '6px'
+  };
 
   // const navigate = useNavigate()
 
@@ -17,8 +28,8 @@ export const Login = () => {
  }
 
  const validationSchema = Yup.object().shape({
-    email: Yup.string().email("Email válido").required("Campo obligatorio"),
-    password: Yup.string().min(6, "6 caracteres mínimo").required("Campo obligatorio")
+    email: Yup.string().email("* Email no válido").required("* Campo obligatorio"),
+    password: Yup.string().min(6, "* Minimo 6 caracteres").required("* Campo obligatorio")
  });
 
  const onSubmit = () => {
@@ -34,31 +45,65 @@ export const Login = () => {
   const {handleSubmit, handleChange, values, errors} = formik
   return (
     <>
-      <section className='auth-page'>
-        <div className='auth-img'><img src="../assets/image-category-2.png" alt="" /></div>
+    <section className='auth-page-container'>
+      <div className='auth-page'>
+        <div className='auth-img'></div>
 
         <div className='auth-container'>
-          <div className="auth">
+          <div className='auth'>
+              <img className='logo' src={logo} alt="brand_logo" />
+              <div className='title'>
+                <h2>Log in to your Account</h2>
+                <p>Welcome back, please enter your details.</p>
+              </div>
+
             <form onSubmit={handleSubmit}>
-              <h1> Log In</h1>
-              <div>
-                <label>Email</label>
-                <input onChange={handleChange} value={values.email} name="email" />
+              <div className='input-section'>
+                <div className='social-container'>
+                  <img src="../assets/facebook-logo.png" alt="" />
+                  <img src="../assets/google-logo.png" alt="" />
+                </div>
+
+                <div className='or-container'>
+                  <div className='first-line'></div>
+                  OR
+                  <div className='second-line'></div>
+                </div>
+
+                <div className='input-container'>
+                  <label>Email</label>
+                  <input className='text-input' onChange={handleChange} value={values.email} name="email" />
+                </div>
+                  {errors.email && <div className="error-color">{errors.email}</div>}
+
+                <div className='input-container'> 
+                  <label>Password</label>
+                  <input className='text-input' onChange={handleChange} value={values.password} name="password" type="password"/>
+                </div>
+                  {errors.password && <div className="error-color">{errors.password}</div>}
+
+                  <div className='options-container'>
+                    <div className='opt-in-container'>
+                      <input type="checkbox" name="my-checkbox" id="opt-in" />
+                      <label for="opt-in">Remember me</label>
+                    </div>
+                    <label>Forgot Password?</label>
+                  </div>
               </div>
-                {errors.email && <div className="error-color">{errors.email}</div>}
-              <div> 
-                <label>Password</label>
-                <input onChange={handleChange} value={values.password} name="password" type="password"/>
+
+              <div className='button-container'>
+                <button type="submit">Log in</button>
               </div>
-                {errors.password && <div className="error-color">{errors.password}</div>}
-              <button type="submit">Login</button>
-              <div>
-                <Link to="/register"> Don't have an account? Create one! </Link>
+              <div className='sign-up'>
+                <label>Don’t have an account?</label>
+                <Link style={linkStyle} to="/register">Sign Up</Link>
               </div>
+
             </form>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
     </>
   )
 }
