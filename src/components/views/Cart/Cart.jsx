ac1be3e053cbfef9411d9956bfb727a090a79304
Tincho develops <martinlabastie.p@gmail.com>
2022-09-04 18:26:08 -0300

    created redux dir and files

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
 
 export const Cart = () => {
+
+  const dispatch = useDispatch()
+  
+  const {error, loading, cartReducer} = useSelector(state=>{
+    console.log(state)
+    return state.cartReducer
+  })
+
   return (
-    <div>Cart</div>
+    <div>
+      Cart
+    </div>
   )
 }
 
