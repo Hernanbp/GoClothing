58d6e98bbd5f93735a0e17cbd1e65e14012ed8b7
Tincho develops <martinlabastie.p@gmail.com>
2022-09-04 18:47:34 -0300

    cart component (consumiendo estado de redux)

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -7,7 +7,8 @@ export const Cart = () => {
   const dispatch = useDispatch()
 
   const {error, loading, cartReducer} = useSelector(state=>{
-    console.log(state)
+    console.log(state)      // TODO el estado de redux
+    console.log(state.cartReducer) // Solo el reducer del Cart (1 de todo el state de redux)
     return state.cartReducer
   })
 
@@ -18,7 +19,8 @@ export const Cart = () => {
       {cartReducer && cartReducer.map(()=>{
         return (
           <div>
-            <ProductsList />
+            card component con el producto
+            {/* <ProductsList /> */}
           </div>
         )
       })}
