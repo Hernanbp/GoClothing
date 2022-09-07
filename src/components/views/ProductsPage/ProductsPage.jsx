4dc3ed54b0ff8e3847ed62a855f7a19a0abc8864
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-09-07 20:54:16 -0300

    fixes styles

--- src/components/views/ProductsPage/ProductsPage.jsx
+++ src/components/views/ProductsPage/ProductsPage.jsx
@@ -10,15 +10,15 @@ export const ProductsPage = () => {
     <h1>ProductsPage</h1>
     <hr />
     {
-                products.map( product => (
-                    <div key={product._id}>
-                        <h2>{product.name}</h2>
-                        <p>{product.description}</p>
-                        <Link to={`/products/${product._id}`}>Más..</Link>
-                    </div>
-                ))
-              }
-              <Link to='/'>Go to Home</Link>
+        products.map( product => (
+            <div key={product._id}>
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+                <Link to={`/products/${product._id}`}>Más..</Link>
+            </div>
+        ))
+      }
+      <Link to='/'>Go to Home</Link>
     </>
   )
 }
