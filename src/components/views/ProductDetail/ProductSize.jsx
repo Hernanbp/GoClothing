8166386a56dede04d3a95f3a0d7ac9edd6c16b3b
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-09-08 09:26:26 -0300

    Update ProductSize.jsx

--- src/components/views/ProductDetail/ProductSize.jsx
+++ src/components/views/ProductDetail/ProductSize.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 export const ProductSize = ({products}) => {
 
     
   const [selected, setSelected] = useState(1)
-  const [size, setSize] = useState(null)
+  const [sizes, setSizes] = useState({
+    first: "",
+    second: "",
+    third: "",
+  })
   // const cat = products.category
 
   // 'pants'
@@ -15,18 +19,23 @@ export const ProductSize = ({products}) => {
   // 'jackets'
 
   
-  // use efect()
+  useEffect(() => {
+    if (products.category === 'pants') {
+      return setSizes({ first: { size: '36"', label: 'Small' }, second: { size: '40"', label: 'Medium' } , third: { size: '42"', label: 'Large' } })
+    }
+    if (products.category === 'shoes') {
+      return setSizes({ first: { size: '38', label: 'EUR' }, second: { size: '42', label: 'EUR' } , third: { size: '44', label: 'EUR' } })
+    }
+    if (products.category === 't-shirts' || products.category === 'jackets' ) {
+      return setSizes({ first: { size: 'S', label: 'Small' }, second: { size: 'M', label: 'Medium' } , third: { size: 'L', label: 'Large' } })
+    }
+    if (products.category === 'purses') {
+      return setSizes({ first: { size: 'Black', label: 'Color' }, second: { size: 'Pink', label: 'Color' } , third: { size: 'Blue', label: 'Color' } })
+    }
+  }, [products.category])
+  
 
-  if (products.category === 'pants') {
-    return setSize({ first: '38"', second: '38"', third: '42"' })
-  }
-  if (products.category === 'shoes') {
-    return setSize({ first: '38', second: '42', third: '44' })
-  }
-  if (products.category === 't-shirts' || products.category === 'jackets' ) {
-    return setSize({ first: 'S', second: 'M', third: 'L' })
-  }
-
+  console.log(sizes);
 
 
 
@@ -34,9 +43,9 @@ export const ProductSize = ({products}) => {
     <>
         <span className='size'>Select Size: <span>Medium</span></span>
         <div className='size-options'>
-            <div className={selected === 1 && "select"} onClick={ () => setSelected(1)}><span>{ size.first }</span><span>Small</span></div>
-            <div className={selected === 2 && "select"} onClick={ () => setSelected(2)}><span>{ size.second }</span><span>Medium</span></div>
-            <div className={selected === 3 && "select"} onClick={ () => setSelected(3)}><span>{ size.third }</span><span>Large</span></div>
+            <div className={selected === 1 && "select"} onClick={ () => setSelected(1)}><span>{ sizes.first.size }</span><span>{sizes.first.label}</span></div>
+            <div className={selected === 2 && "select"} onClick={ () => setSelected(2)}><span>{ sizes.second.size }</span><span>{sizes.second.label}</span></div>
+            <div className={selected === 3 && "select"} onClick={ () => setSelected(3)}><span>{ sizes.third.size }</span><span>{sizes.third.label}</span></div>
         </div>
     </>
   )
