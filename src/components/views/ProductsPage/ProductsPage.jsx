9f25b411e8c43db93566d294e319151445800fc7
tinchopapa56 <martinlabastie.pp@gmail.com>
2022-09-14 19:54:48 -0300

    api POST+GET (en products route)

--- src/components/views/ProductsPage/ProductsPage.jsx
+++ src/components/views/ProductsPage/ProductsPage.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import { ProductCard }from "../ProductCard/ProductCard"
+
 import { Link } from 'react-router-dom'
 import { products } from '../../../data/prducts'
 
 
 
 export const ProductsPage = () => {
+
+  // GET + POST
+    const [dataFromApi, setDataFromApi] = useState([])
+    const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    (async function () {
+      try{
+        const res = await axios.get('https://alkcommerceback.herokuapp.com/products')
+        console.log(res.data)
+        setDataFromApi(res.data)
+        setLoading(false)
+      }
+      catch(err){
+        console.log(err)
+      }
+    })()
+  },[])
+/*
+  const addProducto = ()=>{
+    try {
+      const res = axios.post('https://alkcommerceback.herokuapp.com/products', {
+        "title": "usando el POST FALOP DESDE LA APP",
+        "description": "alta jogineta para inveirno sfaf fsuaobf d asd f sf aefeffsefes efsString",
+        "price": 6501,
+        "image": "String",
+        "category": "pants" 
+      })
+      console.log(res);
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }*/
+
+
   return (
     <>
     <h1>ProductsPage</h1>
     <hr />
-    {
-        products.map( product => (
+      {products.map( product => (
             <div key={product._id}>
                 <h2>{product.name}</h2>
                 <p>{product.description}</p>
@@ -19,6 +56,25 @@ export const ProductsPage = () => {
         ))
       }
       <Link to='/'>Go to Home</Link>
+
+
+    <h1> USANDO LA API:</h1>
+
+      {loading ? "skeleton loading" :         //ESTO TENDRIA Q USAR PRODUCT CARD
+        (dataFromApi.map( (apiProduct) => {
+          return(
+            <div>
+                <h2>{apiProduct.title}</h2>
+                <p>{apiProduct.description}</p>
+                <p>{apiProduct.price}</p>
+                <p>{apiProduct.category}</p>
+                {/* <img src={apiProduct.image} /> */}
+                <Link to={`/products/${apiProduct._id}`}>Más..</Link>
+            </div>
+          )})
+        )
+      }
+    
     </>
   )
 }
