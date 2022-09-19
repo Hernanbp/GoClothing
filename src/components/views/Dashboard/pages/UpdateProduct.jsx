b824eb3ed41160f89cc893780b0dd946f6391796
Hernan <her.cw@hotmail.com>
2022-09-18 22:16:19 -0300

    update product functionality

--- src/components/views/Dashboard/pages/UpdateProduct.jsx
+++ src/components/views/Dashboard/pages/UpdateProduct.jsx
@@ -1,5 +1,69 @@
-import React from "react"
+import axios from "axios"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
 export const UpdateProduct = () => {
-  return <div>UpdateProduct</div>
+  const [products, setProducts] = useState([])
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [price, setPrice] = useState(0)
+  const [id, setId] = useState("")
+
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+
+    const resp = await axios.put(
+      `https://alkcommerceback.herokuapp.com/products/${id}`,
+      {
+        title,
+        description,
+        price,
+        image: "https://picsum.photos/200/300",
+        category: "Clothes",
+      }
+    )
+
+    setProducts([...products, resp.data])
+    console.log(resp.data)
+  }
+
+  return (
+    <form onSubmit={handleUpdate} className="form-dashboard">
+      <h2>Update a Product</h2>
+      <label htmlFor="id">ID</label>
+      <input
+        type="text"
+        name="id"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <label htmlFor="title">Description</label>
+      <textarea
+        type="text"
+        name="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <label htmlFor="title">Price</label>
+      <input
+        type="number"
+        name="price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <div className="buttons-dashboard">
+        <Link to="/dashboard/products">
+          <button>Cancel</button>
+        </Link>
+        <button>Update Product</button>
+      </div>
+    </form>
+  )
 }
