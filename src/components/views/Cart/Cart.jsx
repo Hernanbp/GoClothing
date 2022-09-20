f12b02081f268adf89a6e63db1a3f1efb710de15
tinchopapa56 <martinlabastie.pp@gmail.com>
2022-09-19 23:16:16 -0300

    cart right

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { ProductCard } from '../ProductCard/ProductCard'
 import { Link } from 'react-router-dom'
 import "./styles.css"
+import {Navbar} from "../Navbar/Navbar"
 
 export const Cart = () => {
 
@@ -14,28 +15,40 @@ export const Cart = () => {
   })
 
   return (
-    <div>
-    Cart Route
-    <Link to="/"> Keep Shopping</Link>
-
-    {cart ? 
-      (cart.map((productInCart, i)=>{
-        return (
-            <ProductCard 
-              key={i}
-              title={productInCart.title}
-              image={productInCart.image}
-              // description={productInCart.description}
-              category={productInCart.category}
-              price={productInCart.price}
-              beforePrice ={productInCart.beforePrice}
-              product={productInCart}
-            />
-        )
-      }))
-      :
-      "NO HAY NADA"
-      }
+    <div className='cart'>
+      <Navbar />
+      <h2>Cart</h2>
+      <div> {/*SIN NOMBRE*/}
+        <div className='cart-left'>
+          {cart ? (cart.map((productInCart, i)=>{
+            return (
+                <ProductCard 
+                  key={i}
+                  title={productInCart.title}
+                  image={productInCart.image}
+                  category={productInCart.category}
+                  price={productInCart.price}
+                  product={productInCart}
+                />
+            )
+          }))
+          : <h3>NO HAY NADA</h3>
+          }
+        </div>
+        <div className="cart-right">
+          <h3>Cart totals</h3>
+          <div>
+            <p>SUBTOTAL</p>
+            <p>2000(hard)</p>
+          </div>
+          <div>
+            <p>TOTAL</p>
+            <p>3800(hard)</p>
+          </div>
+          <button>Proceed to checkout</button>
+        </div>
+      </div>
+  
 
     </div>
   )
