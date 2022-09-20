26ec3db48e7c6860a67faca55d6cd5fa6d8915eb
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-09-20 20:58:44 -0300

    add Style cart

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -18,8 +18,11 @@ export const Cart = () => {
   return (
     <div className='cart'>
       <Navbar />
-      <h2>Cart</h2>
-      <div> {/*SIN NOMBRE*/}
+      <h2>Your Cart</h2>
+      {
+        /* algun icono de un carrito */
+      }
+      <div className='cart-container'>
         <table className='cart-left'> 
           <tbody>
             <tr>
@@ -31,20 +34,29 @@ export const Cart = () => {
               <td>CATEGORY</td>
               <td>SUBTOTAL</td>
             </tr>
-            {cart ? (cart.map((productInCart, i)=>{
+            {cart === "null" ? (cart.map((productInCart, i)=>{
               return (
                 <tr>
                   <td onClick={()=>{console.log("sacar del carrito")}}>X</td>
-                  <td><img src={productInCart.image} style={{width:"50%"}}/></td>
+                  <td><img src={productInCart.image} style={{width:"50%"}} alt=""/></td>
                   <td>{productInCart.title}</td>
                   <td>{productInCart.price}$</td>
                   <td>{productInCart.category}</td>
                   <td>1=</td>
                   <td>4500$=</td>
-                  </tr>
+                </tr>
               )
             }))
-            : <h3>NO HAY NADA</h3>
+            :
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><h3>Sorry, the cart is empty!</h3></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
             }
           </tbody>
         </table>
