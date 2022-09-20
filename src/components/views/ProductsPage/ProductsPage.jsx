d9788f011c8a1dc8cf5375e1c87e5a35d815d3f3
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-09-20 19:17:14 -0300

    fixes filter category and search

--- src/components/views/ProductsPage/ProductsPage.jsx
+++ src/components/views/ProductsPage/ProductsPage.jsx
@@ -1,18 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import axios from "axios";
 import { ProductCard }from "../ProductCard/ProductCard"
 import { Navbar } from "../../views"
 import "./styles.css"
 import { SkeletonCard } from '../ProductCard/SkeletonCard';
+import debounce from 'lodash.debounce'
 
 
 
 export const ProductsPage = () => {
 
   // GET + POST
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState("")
+  const [renderList, setRenderList] = useState(null)
+  const [catFilter, setCatFilter] = useState(null)
+  const searchInput = useRef()
 
+  
   useEffect(() => {
     (async function () {
       try{
@@ -26,6 +32,22 @@ export const ProductsPage = () => {
       }
     })()
   },[])
+  
+
+  useEffect(() => {
+    if (search) {
+      let i = search.toLocaleLowerCase()
+      catFilter !== null ?
+      setRenderList(catFilter.filter(data => data.title.toLocaleLowerCase().startsWith(i)))
+      :
+      setRenderList(products.filter(data => data.title.toLocaleLowerCase().startsWith(i)))
+    } else {
+      catFilter !== null ?
+      setRenderList(catFilter)
+      :
+      setRenderList(products)
+    }
+  }, [search, products, catFilter])
 
 /*
   const addProducto = ()=>{
@@ -44,6 +66,27 @@ export const ProductsPage = () => {
     }
   }*/
 
+  const handleSearch = debounce((event) => {
+    setSearch(event.target.value)
+  }, 1000)
+
+  const handleCaregory = (event) => {
+    let filter = products.filter(data => (data.category.toLocaleLowerCase() === event.target.innerHTML.toLowerCase()))
+    setRenderList(filter)
+    setCatFilter(filter)
+  }
+
+  const handleClear = (event) => {
+    setRenderList(products)
+    setSearch("")
+    setCatFilter(null)
+    searchInput.current.value = ""
+  }
+
+  // poner negrita en la cat seleccionada
+  // arreglar buscador + categoria
+  
+  console.log(renderList);
 
   return (
     <>
@@ -53,35 +96,41 @@ export const ProductsPage = () => {
         <div className='controler'>
           <div>
             <h3>Search</h3>
-           <input type="text" />
+           <input
+            ref={searchInput}
+            type="text"
+            onChange={handleSearch}
+           />
           </div>
-
-          <div>
-            <h3>Maximum price</h3>
-            <input type="range" />
-          </div>
-
           <div>
             <h3>Categories</h3>
             <ul>
-              <li>Pants</li>
-              <li>Shoes</li>
-              <li>Purses</li>
-              <li>T-Shirts</li>
-              <li>Jackets</li>
+              <li value="pants" onClick={ (event) => handleCaregory(event)}>Pants</li>
+              <li value="shoes" onClick={handleCaregory}>Shoes</li>
+              <li value="purses" onClick={handleCaregory}>Purses</li>
+              <li value="t-shirts" onClick={handleCaregory}>T-Shirts</li>
+              <li value="jackets" onClick={handleCaregory}>Jackets</li>
             </ul>
           </div>
+          <button
+            onClick={handleClear}
+          >
+            Clear Filters</button>
 
         </div>
 
 
         <div className='product-list'>
+          {
+            renderList?.length === 0 && !loading && <div>No se encontraron productos</div>
+          }
+          
           { !loading ?
-            (products.map( (products) => {
+            (renderList.map( (products) => {
               return(
                 <ProductCard products={products}/>
               )})
-              ) 
+              )
               :
               <>
                 <SkeletonCard />
